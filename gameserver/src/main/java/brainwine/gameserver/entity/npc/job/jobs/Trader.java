93d28a3b0c309fa7214043e0f771c976217e0a22
Mert Bora İnevi <mertborainevi@gmail.com>
2025-09-30 01:24:31 +0200

    show offer item button conditionally

--- gameserver/src/main/java/brainwine/gameserver/entity/npc/job/jobs/Trader.java
+++ gameserver/src/main/java/brainwine/gameserver/entity/npc/job/jobs/Trader.java
@@ -169,7 +169,11 @@ public class Trader extends DialoguerJob {
 
         dialog.addSection(TradeSession.Dialogs.createQuantitySelector(player, item).setText(item.getShillingsPrice() > 0 ? "How many are you selling?" : "How many are you giving?"));
 
-        acceptItemLinkToScrapMarket(dialog);
+        if(barterSkill < 6) {
+            dialog.addSection(new DialogSection().setText("When you reach barter level 6, you will also be able to list this on the Scrap Market and possibly get a better offer there."));
+        } else {
+            acceptItemLinkToScrapMarket(dialog);
+        }
 
         player.showDialog(dialog, ans -> {
             if(acceptItemHandleScrapMarket(me, player, item, ans)) return;
