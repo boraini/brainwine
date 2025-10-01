b6f4d0210000c8c03421d634a31c70f7f77cb95b
Mert Bora İnevi <mertborainevi@gmail.com>
2025-10-02 00:53:02 +0200

    make order of operations identical to the client

--- gameserver/src/main/java/brainwine/gameserver/player/Player.java
+++ gameserver/src/main/java/brainwine/gameserver/player/Player.java
@@ -1119,7 +1119,7 @@ public class Player extends Entity implements CommandExecutor {
     
     public double getPlacementRange() {
         double accessoryBonus = getInventory().findAccessoryWithUse(ItemUseType.BUILDING_EXTENSION).isAir() ? 1.0 : 2.0;
-        return Math.ceil(accessoryBonus * MathUtils.lerp(5.0, 13.0, (double)getTotalSkillLevel(Skill.BUILDING) / MAX_SKILL_LEVEL));
+        return accessoryBonus * Math.ceil(MathUtils.lerp(5.0, 13.0, (double)getTotalSkillLevel(Skill.BUILDING) / MAX_SKILL_LEVEL));
     }
     
     public int getMaxTargetableEntities() {
