303fa1261b74f9cc15389ee391d94654c9cbcd93
Mert Bora İnevi <mertborainevi@gmail.com>
2026-01-10 20:48:45 +0100

    prevent airships from going into the ground

--- gameserver/src/main/java/brainwine/gameserver/zone/gen/sky/StructureSkyDecorator.java
+++ gameserver/src/main/java/brainwine/gameserver/zone/gen/sky/StructureSkyDecorator.java
@@ -23,13 +23,15 @@ public class StructureSkyDecorator extends SkyDecorator {
     public static void place(Prefab prefab, GeneratorContext ctx, int x, int y) {
         if(prefab == null) return;
         int clearance = 2;
+        int surfaceSampleCenter = Math.max(0, Math.min(ctx.getWidth(), x + prefab.getWidth() / 2));
+        int maxYSurface = Math.max(clearance, ctx.getSurface(surfaceSampleCenter) - prefab.getHeight());
         int minX = Math.max(clearance, x - prefab.getWidth() + 1);
         int maxX = Math.min(ctx.getWidth() - prefab.getWidth() - clearance, x);
-        int minY = Math.max(clearance, y - prefab.getHeight() + 1);
-        int maxY = Math.min(ctx.getHeight() - prefab.getHeight() - clearance, y);
+        int minY = Math.min(maxYSurface, Math.max(clearance, y - prefab.getHeight() + 1));
+        int maxY = Math.min(maxYSurface, Math.min(ctx.getHeight() - prefab.getHeight() - clearance, y));
         x = minX + (int)(ctx.nextDouble() * (maxX - minX));
         y = minY + (int)(ctx.nextDouble() * (maxY - minY));
-        ctx.placePrefab(prefab, x, y);
+        if(y >= 0) ctx.placePrefab(prefab, x, y);
     }
 
     public WeightedMap<Prefab> getPrefabs() {
