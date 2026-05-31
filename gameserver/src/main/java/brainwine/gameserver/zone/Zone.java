ab926a3f4e750727dd607e7ffaaa357ca48e6444
Mert Bora İnevi <mertborainevi@gmail.com>
2026-05-31 15:08:42 +0200

    add normal-size world exploration reward

--- gameserver/src/main/java/brainwine/gameserver/zone/Zone.java
+++ gameserver/src/main/java/brainwine/gameserver/zone/Zone.java
@@ -1977,9 +1977,13 @@ public class Zone {
         }
 
         // Reward players who have explored the whole XL world
+        final int SMALL_MEDIUM_THRESHOLD = 1000 * 500;
         final int MEDIUM_LARGE_THRESHOLD = 2000 * 1000;
-        if(currentOverallExplorationProgress < 0.99 && getOverallExplorationProgress() >= 0.99 && explorer != null && getRules().isCanGetExplorationReward() && getWidth() * getHeight() >= MEDIUM_LARGE_THRESHOLD) {
-            Item reward = ItemRegistry.getItem("furniture/obelisk-" + biome.getId());
+        if(currentOverallExplorationProgress < 0.999 && getOverallExplorationProgress() >= 0.999 && explorer != null && getRules().isCanGetExplorationReward()) {
+            Item reward = Item.AIR;
+
+            if(getWidth() * getHeight() >= MEDIUM_LARGE_THRESHOLD) reward = ItemRegistry.getItem("furniture/obelisk-" + biome.getId());
+            else if(getWidth() * getHeight() >= SMALL_MEDIUM_THRESHOLD) reward = ItemRegistry.getItem("furniture/flag-" + biome.getId());
 
             if(!reward.isAir()) {
                 explorer.notify("You receive "
